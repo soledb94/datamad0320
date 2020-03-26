c256560c00aa9bc38c1ada306f5aa66b995f7d80
Julio Zumarraga <soledominguezbarbadillo@gmail.com>
2020-03-26 18:59:40 +0100

    avance

--- module-1/lab-errhand_listcomp/your-code/main.py
+++ module-1/lab-errhand_listcomp/your-code/main.py
@@ -6,21 +6,26 @@ my_listComprehension = [1/egg for egg in eggs]
 
 print(my_listComprehension)
 
-"""
-
 #Insert here the module/library import statements 
+#import statements 
+import math
+import os 
+import random
 
-
-
+"""
 
 #1. Calculate the square number of the first 20 numbers. Use square as the name of the list.
 # Remember to use list comprehensions and to print your results
 
-
+square=[e*e for e in range(21)]
+print(square)
 
 
 #2. Calculate the first 50 power of two. Use power_of_two as the name of the list.
 # Remember to use list comprehensions and to print your results
+
+power_of_two=[2**e for e in range (51)]
+print (power_of_two)
 
 
 
@@ -28,25 +33,27 @@ print(my_listComprehension)
 #3. Calculate the square root of the first 100 numbers. Use sqrt as the name of the list.
 # You will probably need to install math library with pip and import it in this file.  
 # Remember to use list comprehensions and to print your results
-
-
+sqrt=[math.sqrt(e) for e in range(101)]
+print(sqrt)
 
 
 #4. Create this list [-10,-9,-8,-7,-6,-5,-4,-3,-2,-1,0]. Use my_list as the name of the list.
 # Remember to use list comprehensions and to print your results
-
-
-
+my_list=[e for e in range(-11,1)]
+print(my_list)
 
 #5. Find the odd numbers from 1-100. Use odds as the name of the list. 
 # Remember to use list comprehensions and to print your results
 
-
+odd_numbers = [i for i in range(2,101) if (i%2!=0 or i==2) and (i%3!=0 or i==3) and (i%5!=0 or i==5) and (i%7!=0 or i==7)]
+print (odd_numbers)
 
 
 #6. Find all of the numbers from 1-1000 that are divisible by 7. Use divisible_by_seven as the name of the list.
 # Remember to use list comprehensions and to print your results
 
+divisible_by_seven=[e for e in range(1,1001) if e%7==0]
+print(divisible_by_seven)
 
 
 
@@ -56,21 +63,27 @@ print(my_listComprehension)
 
 teststring = 'Find all of the words in a string that are monosyllabic'
 
+vowels=["a","e","i","o","u"]
 
+non_vowels=[e for e in teststring if e not in vowels]
+print(non_vowels)
 
 
 #8. Find the capital letters (and not white space) in the sentence 'The Quick Brown Fox Jumped Over The Lazy Dog'. 
 # Use capital_letters as the name of the list.  
 # Remember to use list comprehensions and to print your results
+sentence="The Quick Brown Fox Jumped Over The Lazy Dog"
 
-
+capital_letters=[e for e in sentence if e.isupper() and e!=" "]
+print(capital_letters)
 
 
 #9. Find all the consonants in the sentence 'The quick brown fox jumped over the lazy dog'.
 # Use consonants as the name of the list.
 # Remember to use list comprehensions and to print your results.
 
-
+consonants=[e for e in sentence if e not in vowels]
+print(consonants)
 
 
 
@@ -78,19 +91,33 @@ teststring = 'Find all of the words in a string that are monosyllabic'
 # You will probably need to import os library and some of its modules. You will need to make some online research.
 # Remember to use list comprehensions and to print your results.
 
+	#os.getcwd()
+al hacer el path en vez de poner user/clara/desktop... existen las ruta relativas con *:
+empieza en user y etc
+
+
+
+
 
 
 #11. Create 4 lists of 10 random numbers between 0 and 100 each. Use random_lists as the name of the list. 
 #You will probably need to import random module
 # Remember to use list comprehensions and to print your results
 
-
+random_list=[e for e in random.choices(range(0,101),k=10)]
+print(random_list,random_list,random_list,random_list)
 
 
 #12. Flatten the following list of lists. Use flatten_list as the name of the output.
 # Remember to use list comprehensions and to print your results
 
 list_of_lists = [[1,2,3],[4,5,6],[7,8,9]]
+
+flatten_list=[x for e in list_of_lists for x in e]
+print(flatten_list)
+
+
+
 
 
 
@@ -101,6 +128,8 @@ list_of_lists = [['40', '20', '10', '30'], ['20', '20', '20', '20', '20', '30', 
 ['30', '20', '30', '50', '10', '30', '20', '20', '20'], ['100', '100'], ['100', '100', '100', '100', '100'], \
 ['100', '100', '100', '100']]
 
+floats=[float(x) for e in list_of_lists for x in e]
+print(floats)
 
 
 
@@ -108,7 +137,12 @@ list_of_lists = [['40', '20', '10', '30'], ['20', '20', '20', '20', '20', '30', 
 
 
 for i in ['a','b','c']:
-    print i**2
+    try:
+        print (i**2)
+    except Exception as err:
+        print("err")
+        break
+    
 
 
 #15. Handle the exception thrown by the code below by using try and except blocks. 
@@ -118,8 +152,11 @@ for i in ['a','b','c']:
 x = 5
 y = 0
 
-z = x/y
-
+try:
+    z = x/y
+except Exception as err:
+    print("All done")
+    print(err,type(err))
 
 
 
@@ -127,7 +164,13 @@ z = x/y
 # Check in provided resources the type of error you may use. 
 
 abc=[10,20,20]
-print(abc[3])
+
+try:
+    print(abc[3])
+except Exception as err:
+    print(err,type(err))
+
+"""
 
 
 #17. Handle at least two kind of different exceptions when dividing a couple of numbers provided by the user. 
@@ -136,12 +179,34 @@ print(abc[3])
 
 
 
+   
+
+
+def division(a,b):
+    if b==0:
+        raise valueError("You should choose a different number")
+    if a!=int or b!=int:
+        raise ValueError("Yoy shoud choose an int as a number")
+    return a/b
+
+while True:
+    try:
+        a= input("Pass a number: ")
+        b = input("Pass other number: ")
+        print(division())
+        break
+    except:
+        print("Try again")
+"""
+
+
 
 #18. Handle the exception thrown by the code below by using try and except blocks. 
 # Check in provided resources the type of error you may use. 
 
 f = open('testfile','r')
 f.write('Test write this')
+
 
 
 
@@ -179,15 +244,4 @@ def linux_interaction():
 
 # 22. Find all of the numbers from 1-1000 that are divisible by any single digit besides 1 (2-9). 
 # Use results as the name of the list 
-
-
-
-
-# 23. Define a customised exception to handle not accepted values. 
-# You have the following user inputs and the Num_of_sections can not be less than 2.
-# Hint: Create a class derived from the pre-defined Exception class in Python
-
-Total_Marks = int(input("Enter Total Marks Scored: ")) 
-Num_of_Sections = int(input("Enter Num of Sections: "))
-
 """
