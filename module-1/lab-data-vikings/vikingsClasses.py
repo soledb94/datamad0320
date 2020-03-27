78c78493a655245fa28f9ed0acb9fc0f50272b04
Julio Zumarraga <soledominguezbarbadillo@gmail.com>
2020-03-27 23:52:55 +0100

    avance

--- module-1/lab-data-vikings/vikingsClasses.py
+++ module-1/lab-data-vikings/vikingsClasses.py
@@ -3,13 +3,42 @@
 
 
 class Soldier:
-    pass
+    def __init__(self, health, strength):
+        self.health=health
+        self.strength=strength 
 
-# Viking
+    def attack(self):
+        return self.strength
+
+    def receiveDamage(self,damage):
+        self.health-=damage
 
 
-class Viking:
-    pass
+class Viking(Soldier):
+    def __init__(self, name,health, strength):
+        self.name=name
+        self.health=health
+        self.strength=strength 
+        
+    def attack(self):
+        super().attack()
+        
+    def receiveDamage(self,damage):
+        self.health-=damage
+        
+        if self.health>1:
+            return f"{self.name} has received {damage} points of damage"
+        if self.health<=0:
+            return f"{self.name} has died in act of combat"
+        
+    def battle_cry(self):
+        return "Odin Owns You All!"
+
+
+
+#class Viking:
+
+    #pass
 
 # Saxon
 
@@ -24,4 +53,3 @@ class War:
     pass
 
 
-s
