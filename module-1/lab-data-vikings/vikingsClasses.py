05cbb3e79d30e09327a0dd68b8f17431b2e05fc3
Julio Zumarraga <soledominguezbarbadillo@gmail.com>
2020-03-29 16:36:55 +0200

    Commit Final

--- module-1/lab-data-vikings/vikingsClasses.py
+++ module-1/lab-data-vikings/vikingsClasses.py
@@ -97,7 +97,6 @@ class War:
             return "Vikings have won the war of the century!"
         elif len(self.vikingArmy)==0:
             return "Saxons have fought for their lives and survive another day..."
-        
         else:
             return "Vikings and Saxons are still in the thick of battle."
         
