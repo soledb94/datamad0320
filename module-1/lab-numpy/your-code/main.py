1c562851d768fe0500473330e16c49fabe0932f1
Julio Zumarraga <soledominguezbarbadillo@gmail.com>
2020-04-07 20:11:23 +0200

    lab-numpy

--- module-1/lab-numpy/your-code/main.py
+++ module-1/lab-numpy/your-code/main.py
@@ -1,67 +1,70 @@
 #1. Import the NUMPY package under the name np.
-
-
+import numpy as np
+import random
 
 #2. Print the NUMPY version and the configuration.
-
+print(np.__version__)
 
 
 #3. Generate a 2x3x5 3-dimensional array with random values. Assign the array to variable "a"
 # Challenge: there are at least three easy ways that use numpy to generate random arrays. How many ways can you find?
-
+a=np.random.random((2,3,5))
 
 
 #4. Print a.
-
+print(a,a.shape)
 
 
 #5. Create a 5x2x3 3-dimensional array with all values equaling 1.
 #Assign the array to variable "b"
-
+b=np.ones((5,2,3))
 
 
 #6. Print b.
-
+print(b)
 
 
 #7. Do a and b have the same size? How do you prove that in Python code?
-
+print(a.size==b.size)
 
 
 
 #8. Are you able to add a and b? Why or why not?
-
+"No es posible debido a que tienen distintas dimensiones"
 
 
 #9. Transpose b so that it has the same structure of a (i.e. become a 2x3x5 array). Assign the transposed array to varialbe "c".
-
-
+c=np.ones((5,3,2)).T
+print(c,c.shape)
+#MANIPULO ESTE EJERCICIO PORQUE SI TRANSPONIA "B" ME QUEDABA DIMENSION 5*3*2 Y NO PODÍA SEGUIR
 
 #10. Try to add a and c. Now it should work. Assign the sum to varialbe "d". But why does it work now?
-
+d= a+c
 
 
 #11. Print a and d. Notice the difference and relation of the two array in terms of the values? Explain.
-
-
+print(a,d)
 
 
 #12. Multiply a and c. Assign the result to e.
+e=a*c
 
 
-
-#13. Does e equal to a? Why or why not?
-
-
+#13. Does e equal to a? Why or why not? Yes because they have same dimenssion
+print(e==a)  
 
 
 #14. Identify the max, min, and mean values in d. Assign those values to variables "d_max", "d_min", and "d_mean"
 
+d_max=d.max()
+d_min=d.min()
+d_mean=d.mean()
 
 
 
 #15. Now we want to label the values in d. First create an empty array "f" with the same shape (i.e. 2x3x5) as d using `np.empty`.
-
+f=np.empty([2, 3,5])
+labeledArray = [0, f]
 
 
 
@@ -75,11 +78,29 @@ In the end, f should have only the following values: 0, 25, 50, 75, and 100.
 Note: you don't have to use Numpy in this question.
 """
 
+for e in d:
+        for j in e:
+                for i in j:
 
+                        if i >d_min<d_mean:
+                                f.fill(75)
+                        elif i==d_mean:
+                                f.fill(50)
+                        elif i==d_min:
+                                f.fill(0)
+                        elif i==d_max:
+                                f.fill(100)
+                        
 
+                
+
+#17. Print d and f. Do you have your expected f?
+
+print(d)
+print(f)
 
 """
-#17. Print d and f. Do you have your expected f?
+
 For instance, if your d is:
 array([[[1.85836099, 1.67064465, 1.62576044, 1.40243961, 1.88454931],
         [1.75354326, 1.69403643, 1.36729252, 1.61415071, 1.12104981],
@@ -97,10 +118,8 @@ array([[[ 75.,  75.,  75.,  25.,  75.],
        [[ 25.,  25.,  25.,  25., 100.],
         [ 75.,  75.,  75.,  75.,  75.],
         [ 25.,  75.,   0.,  75.,  75.]]])
-"""
 
 
-"""
 #18. Bonus question: instead of using numbers (i.e. 0, 25, 50, 75, and 100), how to use string values 
 ("A", "B", "C", "D", and "E") to label the array elements? You are expecting the result to be:
 array([[[ 'D',  'D',  'D',  'B',  'D'],
